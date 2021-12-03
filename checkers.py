57a8f2eb497d649a755e70d7621fa0d936506f8e
Roman20021 <moto260802@list.ru>
2021-12-03 18:05:39 +0300

    checkers

--- checkers.py
+++ checkers.py
@@ -8,7 +8,39 @@ import random
 import numpy as np
 
 
-class Checkers(QMainWindow):
+class MechanicsHeckers:
+    def __init__(self, coordinates_white_checkers, coordinates_black_checkers):
+        self.coordinates_black_checkers = coordinates_black_checkers
+        self.coordinates_white_checkers = coordinates_white_checkers
+        self.checker_btn = None
+        self.cell_btn = None
+
+    def catch_button_checkers(self, btn, x, y, collor):
+        self.checker_btn = (btn, x, y, collor)
+
+    def catch_button_cells(self, btn, x, y):
+        self.cell_btn = (btn, x, y)
+        if self.checker_btn != None:
+            self.change_coordinates()
+
+
+    def change_coordinates(self):
+        if self.cell_btn != None and self.checker_btn != None:
+            x_checker = self.checker_btn[1]
+            y_checker = self.checker_btn[2]
+            x_cell = self.cell_btn[1]
+            y_cell = self.cell_btn[2]
+            if self.checker_btn[3] == 'black':
+                del self.coordinates_black_checkers[(x_checker, y_checker)]
+                self.coordinates_black_checkers[(x_cell, y_cell)] = self.checker_btn[0]
+            if self.checker_btn[3] == 'white':
+                del self.coordinates_black_checkers[(x_checker, y_checker)]
+                self.coordinates_black_checkers[(x_cell, y_cell)] = self.checker_btn[0]
+            self.checker_btn = None
+            self.cell_btn = None
+
+
+class GuiHeckers(QMainWindow):
     def __init__(self):
         super().__init__()
         self.game_size = (8, 8)
@@ -17,8 +49,9 @@ class Checkers(QMainWindow):
         self.number_black_checkers = 12
         self.number_white_checkers = 12
         self.get_first_coordinates()
-        self.get_board_gui()
-        self.get_checkers_gui()
+        self.get_gui()
+        self.mechanic = MechanicsHeckers(self.coordinates_white_checkers, self.coordinates_black_checkers)
+        self.push_button()
 
     def get_first_coordinates(self):
         for i in range(8):
@@ -31,47 +64,59 @@ class Checkers(QMainWindow):
                 if i % 2 == 0 and j % 2 != 0 or i % 2 != 0 and j % 2 == 0:
                     self.coordinates_white_checkers[(i, j)] = None
 
-    def get_checkers_gui(self):
-        self.white_checkers_btn = np.zeros(self.game_size, dtype=QPushButton)
-        self.black_checkers_btn = np.zeros(self.game_size, dtype=QPushButton)
-        for i in range(self.game_size[0]):
-            for j in range(self.game_size[1]):
-                if (i, j) in list(self.coordinates_white_checkers.keys()):
-                    btn = QPushButton(f'', self)
-                    btn.setGeometry(i * 100, j * 100, 50, 50)
-                    btn.setStyleSheet(
-                        "background-color: white; font-size: 40px; font-weight: bold")
-                    self.white_checkers_btn[i, j] = btn
-                if (i, j) in list(self.coordinates_black_checkers.keys()):
-                    btn = QPushButton(f'', self)
-                    btn.setGeometry(i * 100, j * 100, 50, 50)
-                    btn.setStyleSheet(
-                        "background-color: yellow; font-size: 40px; font-weight: bold, background-image : url(unnamed.png);")
-                    self.black_checkers_btn[i, j] = btn
-        self.showFullScreen()
-
-    def get_board_gui(self):
+    def get_gui(self):
+        self.white_checkers_btns = np.zeros(self.game_size, dtype=QPushButton)
+        self.black_checkers_btns = np.zeros(self.game_size, dtype=QPushButton)
         self.btns = np.zeros(self.game_size, dtype=QPushButton)
+
         for i in range(self.game_size[0]):
             for j in range(self.game_size[1]):
                 btn = QPushButton(f'', self)
                 btn.setGeometry(i * 100, j * 100, 100, 100)
-                btn = self.paint_over(btn, i, j)
+                btn = self._paint_over(btn, i, j)
+                btn.clicked.connect(
+                    lambda state, obj=btn, i=i, j=j: self.mechanic.catch_button_cells(obj, i, j))
+                if (i, j) in list(self.coordinates_white_checkers.keys()):
+                    btn_checker = self._paint_checkers('white', i, j)
+                    self.white_checkers_btns[i, j] = btn_checker
+                    btn_checker.clicked.connect(
+                        lambda state, obj=btn_checker: self.mechanic.catch_button_checkers(obj, i, j, 'white'))
+                if (i, j) in list(self.coordinates_black_checkers.keys()):
+                    btn_checker = self._paint_checkers('black', i, j)
+                    self.black_checkers_btns[i, j] = btn_checker
+                    btn_checker.clicked.connect(
+                        lambda state, obj=btn_checker, i=i, j=j: self.mechanic.catch_button_checkers(obj, i, j,
+                                                                                                     'black'))
                 self.btns[i][j] = btn
+        self.showFullScreen()
 
-    def paint_over(self, btn, i, j):
+    def _paint_over(self, btn, i, j):
         if i % 2 == 0 and j % 2 == 0:
             btn.setStyleSheet("background-color: white; font-size: 20px; font-weight: bold")
         if i % 2 == 0 and j % 2 != 0:
-            btn.setStyleSheet("background-color: black; font-size: 20px; font-weight: bold")
+            btn.setStyleSheet("background-color: gray; font-size: 20px; font-weight: bold")
         if i % 2 != 0 and j % 2 != 0:
             btn.setStyleSheet("background-color: white; font-size: 20px; font-weight: bold")
         if i % 2 != 0 and j % 2 == 0:
-            btn.setStyleSheet("background-color: black; font-size: 20px; font-weight: bold")
+            btn.setStyleSheet("background-color: gray; font-size: 20px; font-weight: bold")
         return btn
+
+    def _paint_checkers(self, collor, i, j):
+        btn = QPushButton(f'', self)
+        btn.setGeometry(i * 100, j * 100, 50, 50)
+        btn.setStyleSheet(
+            f"background-color: {collor}; font-size: 40px; font-weight: bold")
+        return btn
+
+    def push_button(self):
+
+        if self.mechanic.checker_btn != None and self.mechanic.cell_btn != None:
+            self.coordinates_black_checkers = self.mechanic.coordinates_black_checkers
+            self.coordinates_white_checkers = self.mechanic.coordinates_white_checkers
+            self.get_gui()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Checkers()
+    ex = GuiHeckers()
     sys.exit(app.exec_())
