8f2ac9054f1e6a46b2bf199834c8a97baa74c356
Roman20021 <moto260802@list.ru>
2021-12-04 15:38:44 +0300

    lol

--- checkers.py
+++ checkers.py
@@ -8,50 +8,18 @@ import random
 import numpy as np
 
 
-class MechanicsHeckers:
-    def __init__(self, coordinates_white_checkers, coordinates_black_checkers):
-        self.coordinates_black_checkers = coordinates_black_checkers
-        self.coordinates_white_checkers = coordinates_white_checkers
-        self.checker_btn = None
-        self.cell_btn = None
-
-    def catch_button_checkers(self, btn, x, y, collor):
-        self.checker_btn = (btn, x, y, collor)
-
-    def catch_button_cells(self, btn, x, y):
-        self.cell_btn = (btn, x, y)
-        if self.checker_btn != None:
-            self.change_coordinates()
-
-
-    def change_coordinates(self):
-        if self.cell_btn != None and self.checker_btn != None:
-            x_checker = self.checker_btn[1]
-            y_checker = self.checker_btn[2]
-            x_cell = self.cell_btn[1]
-            y_cell = self.cell_btn[2]
-            if self.checker_btn[3] == 'black':
-                del self.coordinates_black_checkers[(x_checker, y_checker)]
-                self.coordinates_black_checkers[(x_cell, y_cell)] = self.checker_btn[0]
-            if self.checker_btn[3] == 'white':
-                del self.coordinates_black_checkers[(x_checker, y_checker)]
-                self.coordinates_black_checkers[(x_cell, y_cell)] = self.checker_btn[0]
-            self.checker_btn = None
-            self.cell_btn = None
-
-
 class GuiHeckers(QMainWindow):
     def __init__(self):
         super().__init__()
         self.game_size = (8, 8)
         self.coordinates_black_checkers = {}
         self.coordinates_white_checkers = {}
+        self.checker_btn = None
+        self.cell_btn = None
         self.number_black_checkers = 12
         self.number_white_checkers = 12
         self.get_first_coordinates()
         self.get_gui()
-        self.mechanic = MechanicsHeckers(self.coordinates_white_checkers, self.coordinates_black_checkers)
-        self.push_button()
 
     def get_first_coordinates(self):
         for i in range(8):
@@ -75,18 +43,19 @@ class GuiHeckers(QMainWindow):
                 btn.setGeometry(i * 100, j * 100, 100, 100)
                 btn = self._paint_over(btn, i, j)
                 btn.clicked.connect(
-                    lambda state, obj=btn, i=i, j=j: self.mechanic.catch_button_cells(obj, i, j))
+                    lambda state, obj=btn, i=i, j=j: self.catch_button_cells(obj, i, j))
                 if (i, j) in list(self.coordinates_white_checkers.keys()):
                     btn_checker = self._paint_checkers('white', i, j)
                     self.white_checkers_btns[i, j] = btn_checker
+                    self.coordinates_white_checkers[(i, j)] = btn_checker
                     btn_checker.clicked.connect(
-                        lambda state, obj=btn_checker: self.mechanic.catch_button_checkers(obj, i, j, 'white'))
+                        lambda state, obj=btn_checker, i=i, j=j: self.catch_button_checkers(obj, i, j, 'white'))
                 if (i, j) in list(self.coordinates_black_checkers.keys()):
                     btn_checker = self._paint_checkers('black', i, j)
                     self.black_checkers_btns[i, j] = btn_checker
+                    self.coordinates_black_checkers[(i, j)] = btn_checker
                     btn_checker.clicked.connect(
-                        lambda state, obj=btn_checker, i=i, j=j: self.mechanic.catch_button_checkers(obj, i, j,
-                                                                                                     'black'))
+                        lambda state, obj=btn_checker, i=i, j=j: self.catch_button_checkers(obj, i, j, 'black'))
                 self.btns[i][j] = btn
         self.showFullScreen()
 
@@ -108,12 +77,35 @@ class GuiHeckers(QMainWindow):
             f"background-color: {collor}; font-size: 40px; font-weight: bold")
         return btn
 
-    def push_button(self):
+    def catch_button_checkers(self, btn, x, y, collor):
+        self.checker_btn = (btn, x, y, collor)
 
-        if self.mechanic.checker_btn != None and self.mechanic.cell_btn != None:
-            self.coordinates_black_checkers = self.mechanic.coordinates_black_checkers
-            self.coordinates_white_checkers = self.mechanic.coordinates_white_checkers
-            self.get_gui()
+    def catch_button_cells(self, btn, x, y):
+        self.cell_btn = (btn, x, y)
+        if self.checker_btn != None:
+            self.change_coordinates()
+        else:
+            self.checker_btn = None
+            self.cell_btn = None
+
+    def change_coordinates(self):
+        if self.cell_btn != None and self.checker_btn != None and self.cell_btn not in self.coordinates_black_checkers and self.cell_btn not in self.coordinates_white_checkers:#отдельный метод с условием должно быть
+            x_checker = self.checker_btn[1]
+            y_checker = self.checker_btn[2]
+            x_cell = self.cell_btn[1]
+            y_cell = self.cell_btn[2]
+            self.checker_btn[0].deleteLater()
+            btn = self._paint_checkers(self.checker_btn[-1], x_cell, y_cell)
+            btn.show()
+            if self.checker_btn[3] == 'black':
+                del self.coordinates_black_checkers[(x_checker, y_checker)]
+                self.coordinates_black_checkers[(x_cell, y_cell)] = btn
+            if self.checker_btn[3] == 'white':
+                del self.coordinates_white_checkers[(x_checker, y_checker)]
+                self.coordinates_white_checkers[(x_cell, y_cell)] = btn
+            self.checker_btn = None
+            self.cell_btn = None
+
 
 
 if __name__ == '__main__':
